df35177f3a3053edf8ebcc479dd8e75b88d12542
jggarcon <joaogabrielgarcon@gmail.com>
2025-09-18 22:31:33 -0300

    added function to filter for min quantity on order

--- wp-content/themes/hello-elementor/functions.php
+++ wp-content/themes/hello-elementor/functions.php
@@ -5,52 +5,53 @@
  * @package HelloElementor
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
+if (!defined('ABSPATH')) {
 	exit; // Exit if accessed directly.
 }
 
-define( 'HELLO_ELEMENTOR_VERSION', '3.4.4' );
-define( 'EHP_THEME_SLUG', 'hello-elementor' );
+define('HELLO_ELEMENTOR_VERSION', '3.4.4');
+define('EHP_THEME_SLUG', 'hello-elementor');
 
-define( 'HELLO_THEME_PATH', get_template_directory() );
-define( 'HELLO_THEME_URL', get_template_directory_uri() );
-define( 'HELLO_THEME_ASSETS_PATH', HELLO_THEME_PATH . '/assets/' );
-define( 'HELLO_THEME_ASSETS_URL', HELLO_THEME_URL . '/assets/' );
-define( 'HELLO_THEME_SCRIPTS_PATH', HELLO_THEME_ASSETS_PATH . 'js/' );
-define( 'HELLO_THEME_SCRIPTS_URL', HELLO_THEME_ASSETS_URL . 'js/' );
-define( 'HELLO_THEME_STYLE_PATH', HELLO_THEME_ASSETS_PATH . 'css/' );
-define( 'HELLO_THEME_STYLE_URL', HELLO_THEME_ASSETS_URL . 'css/' );
-define( 'HELLO_THEME_IMAGES_PATH', HELLO_THEME_ASSETS_PATH . 'images/' );
-define( 'HELLO_THEME_IMAGES_URL', HELLO_THEME_ASSETS_URL . 'images/' );
+define('HELLO_THEME_PATH', get_template_directory());
+define('HELLO_THEME_URL', get_template_directory_uri());
+define('HELLO_THEME_ASSETS_PATH', HELLO_THEME_PATH . '/assets/');
+define('HELLO_THEME_ASSETS_URL', HELLO_THEME_URL . '/assets/');
+define('HELLO_THEME_SCRIPTS_PATH', HELLO_THEME_ASSETS_PATH . 'js/');
+define('HELLO_THEME_SCRIPTS_URL', HELLO_THEME_ASSETS_URL . 'js/');
+define('HELLO_THEME_STYLE_PATH', HELLO_THEME_ASSETS_PATH . 'css/');
+define('HELLO_THEME_STYLE_URL', HELLO_THEME_ASSETS_URL . 'css/');
+define('HELLO_THEME_IMAGES_PATH', HELLO_THEME_ASSETS_PATH . 'images/');
+define('HELLO_THEME_IMAGES_URL', HELLO_THEME_ASSETS_URL . 'images/');
 
-if ( ! isset( $content_width ) ) {
+if (!isset($content_width)) {
 	$content_width = 800; // Pixels.
 }
 
-if ( ! function_exists( 'hello_elementor_setup' ) ) {
+if (!function_exists('hello_elementor_setup')) {
 	/**
 	 * Set up theme support.
 	 *
 	 * @return void
 	 */
-	function hello_elementor_setup() {
-		if ( is_admin() ) {
+	function hello_elementor_setup()
+	{
+		if (is_admin()) {
 			hello_maybe_update_theme_version_in_db();
 		}
 
-		if ( apply_filters( 'hello_elementor_register_menus', true ) ) {
-			register_nav_menus( [ 'menu-1' => esc_html__( 'Header', 'hello-elementor' ) ] );
-			register_nav_menus( [ 'menu-2' => esc_html__( 'Footer', 'hello-elementor' ) ] );
+		if (apply_filters('hello_elementor_register_menus', true)) {
+			register_nav_menus(['menu-1' => esc_html__('Header', 'hello-elementor')]);
+			register_nav_menus(['menu-2' => esc_html__('Footer', 'hello-elementor')]);
 		}
 
-		if ( apply_filters( 'hello_elementor_post_type_support', true ) ) {
-			add_post_type_support( 'page', 'excerpt' );
+		if (apply_filters('hello_elementor_post_type_support', true)) {
+			add_post_type_support('page', 'excerpt');
 		}
 
-		if ( apply_filters( 'hello_elementor_add_theme_support', true ) ) {
-			add_theme_support( 'post-thumbnails' );
-			add_theme_support( 'automatic-feed-links' );
-			add_theme_support( 'title-tag' );
+		if (apply_filters('hello_elementor_add_theme_support', true)) {
+			add_theme_support('post-thumbnails');
+			add_theme_support('automatic-feed-links');
+			add_theme_support('title-tag');
 			add_theme_support(
 				'html5',
 				[
@@ -67,72 +68,75 @@ if ( ! function_exists( 'hello_elementor_setup' ) ) {
 			add_theme_support(
 				'custom-logo',
 				[
-					'height'      => 100,
-					'width'       => 350,
+					'height' => 100,
+					'width' => 350,
 					'flex-height' => true,
-					'flex-width'  => true,
+					'flex-width' => true,
 				]
 			);
-			add_theme_support( 'align-wide' );
-			add_theme_support( 'responsive-embeds' );
+			add_theme_support('align-wide');
+			add_theme_support('responsive-embeds');
 
 			/*
 			 * Editor Styles
 			 */
-			add_theme_support( 'editor-styles' );
-			add_editor_style( 'editor-styles.css' );
+			add_theme_support('editor-styles');
+			add_editor_style('editor-styles.css');
 
 			/*
 			 * WooCommerce.
 			 */
-			if ( apply_filters( 'hello_elementor_add_woocommerce_support', true ) ) {
+			if (apply_filters('hello_elementor_add_woocommerce_support', true)) {
 				// WooCommerce in general.
-				add_theme_support( 'woocommerce' );
+				add_theme_support('woocommerce');
 				// Enabling WooCommerce product gallery features (are off by default since WC 3.0.0).
 				// zoom.
-				add_theme_support( 'wc-product-gallery-zoom' );
+				add_theme_support('wc-product-gallery-zoom');
 				// lightbox.
-				add_theme_support( 'wc-product-gallery-lightbox' );
+				add_theme_support('wc-product-gallery-lightbox');
 				// swipe.
-				add_theme_support( 'wc-product-gallery-slider' );
+				add_theme_support('wc-product-gallery-slider');
 			}
 		}
 	}
 }
-add_action( 'after_setup_theme', 'hello_elementor_setup' );
+add_action('after_setup_theme', 'hello_elementor_setup');
 
-function hello_maybe_update_theme_version_in_db() {
+function hello_maybe_update_theme_version_in_db()
+{
 	$theme_version_option_name = 'hello_theme_version';
 	// The theme version saved in the database.
-	$hello_theme_db_version = get_option( $theme_version_option_name );
+	$hello_theme_db_version = get_option($theme_version_option_name);
 
 	// If the 'hello_theme_version' option does not exist in the DB, or the version needs to be updated, do the update.
-	if ( ! $hello_theme_db_version || version_compare( $hello_theme_db_version, HELLO_ELEMENTOR_VERSION, '<' ) ) {
-		update_option( $theme_version_option_name, HELLO_ELEMENTOR_VERSION );
+	if (!$hello_theme_db_version || version_compare($hello_theme_db_version, HELLO_ELEMENTOR_VERSION, '<')) {
+		update_option($theme_version_option_name, HELLO_ELEMENTOR_VERSION);
 	}
 }
 
-if ( ! function_exists( 'hello_elementor_display_header_footer' ) ) {
+if (!function_exists('hello_elementor_display_header_footer')) {
 	/**
 	 * Check whether to display header footer.
 	 *
 	 * @return bool
 	 */
-	function hello_elementor_display_header_footer() {
+	function hello_elementor_display_header_footer()
+	{
 		$hello_elementor_header_footer = true;
 
-		return apply_filters( 'hello_elementor_header_footer', $hello_elementor_header_footer );
+		return apply_filters('hello_elementor_header_footer', $hello_elementor_header_footer);
 	}
 }
 
-if ( ! function_exists( 'hello_elementor_scripts_styles' ) ) {
+if (!function_exists('hello_elementor_scripts_styles')) {
 	/**
 	 * Theme Scripts & Styles.
 	 *
 	 * @return void
 	 */
-	function hello_elementor_scripts_styles() {
-		if ( apply_filters( 'hello_elementor_enqueue_style', true ) ) {
+	function hello_elementor_scripts_styles()
+	{
+		if (apply_filters('hello_elementor_enqueue_style', true)) {
 			wp_enqueue_style(
 				'hello-elementor',
 				HELLO_THEME_STYLE_URL . 'reset.css',
@@ -141,7 +145,7 @@ if ( ! function_exists( 'hello_elementor_scripts_styles' ) ) {
 			);
 		}
 
-		if ( apply_filters( 'hello_elementor_enqueue_theme_style', true ) ) {
+		if (apply_filters('hello_elementor_enqueue_theme_style', true)) {
 			wp_enqueue_style(
 				'hello-elementor-theme-style',
 				HELLO_THEME_STYLE_URL . 'theme.css',
@@ -150,7 +154,7 @@ if ( ! function_exists( 'hello_elementor_scripts_styles' ) ) {
 			);
 		}
 
-		if ( hello_elementor_display_header_footer() ) {
+		if (hello_elementor_display_header_footer()) {
 			wp_enqueue_style(
 				'hello-elementor-header-footer',
 				HELLO_THEME_STYLE_URL . 'header-footer.css',
@@ -160,9 +164,9 @@ if ( ! function_exists( 'hello_elementor_scripts_styles' ) ) {
 		}
 	}
 }
-add_action( 'wp_enqueue_scripts', 'hello_elementor_scripts_styles' );
+add_action('wp_enqueue_scripts', 'hello_elementor_scripts_styles');
 
-if ( ! function_exists( 'hello_elementor_register_elementor_locations' ) ) {
+if (!function_exists('hello_elementor_register_elementor_locations')) {
 	/**
 	 * Register Elementor Locations.
 	 *
@@ -170,50 +174,53 @@ if ( ! function_exists( 'hello_elementor_register_elementor_locations' ) ) {
 	 *
 	 * @return void
 	 */
-	function hello_elementor_register_elementor_locations( $elementor_theme_manager ) {
-		if ( apply_filters( 'hello_elementor_register_elementor_locations', true ) ) {
+	function hello_elementor_register_elementor_locations($elementor_theme_manager)
+	{
+		if (apply_filters('hello_elementor_register_elementor_locations', true)) {
 			$elementor_theme_manager->register_all_core_location();
 		}
 	}
 }
-add_action( 'elementor/theme/register_locations', 'hello_elementor_register_elementor_locations' );
+add_action('elementor/theme/register_locations', 'hello_elementor_register_elementor_locations');
 
-if ( ! function_exists( 'hello_elementor_content_width' ) ) {
+if (!function_exists('hello_elementor_content_width')) {
 	/**
 	 * Set default content width.
 	 *
 	 * @return void
 	 */
-	function hello_elementor_content_width() {
-		$GLOBALS['content_width'] = apply_filters( 'hello_elementor_content_width', 800 );
+	function hello_elementor_content_width()
+	{
+		$GLOBALS['content_width'] = apply_filters('hello_elementor_content_width', 800);
 	}
 }
-add_action( 'after_setup_theme', 'hello_elementor_content_width', 0 );
+add_action('after_setup_theme', 'hello_elementor_content_width', 0);
 
-if ( ! function_exists( 'hello_elementor_add_description_meta_tag' ) ) {
+if (!function_exists('hello_elementor_add_description_meta_tag')) {
 	/**
 	 * Add description meta tag with excerpt text.
 	 *
 	 * @return void
 	 */
-	function hello_elementor_add_description_meta_tag() {
-		if ( ! apply_filters( 'hello_elementor_description_meta_tag', true ) ) {
+	function hello_elementor_add_description_meta_tag()
+	{
+		if (!apply_filters('hello_elementor_description_meta_tag', true)) {
 			return;
 		}
 
-		if ( ! is_singular() ) {
+		if (!is_singular()) {
 			return;
 		}
 
 		$post = get_queried_object();
-		if ( empty( $post->post_excerpt ) ) {
+		if (empty($post->post_excerpt)) {
 			return;
 		}
 
-		echo '<meta name="description" content="' . esc_attr( wp_strip_all_tags( $post->post_excerpt ) ) . '">' . "\n";
+		echo '<meta name="description" content="' . esc_attr(wp_strip_all_tags($post->post_excerpt)) . '">' . "\n";
 	}
 }
-add_action( 'wp_head', 'hello_elementor_add_description_meta_tag' );
+add_action('wp_head', 'hello_elementor_add_description_meta_tag');
 
 // Settings page
 require get_template_directory() . '/includes/settings-functions.php';
@@ -221,23 +228,24 @@ require get_template_directory() . '/includes/settings-functions.php';
 // Header & footer styling option, inside Elementor
 require get_template_directory() . '/includes/elementor-functions.php';
 
-if ( ! function_exists( 'hello_elementor_customizer' ) ) {
+if (!function_exists('hello_elementor_customizer')) {
 	// Customizer controls
-	function hello_elementor_customizer() {
-		if ( ! is_customize_preview() ) {
+	function hello_elementor_customizer()
+	{
+		if (!is_customize_preview()) {
 			return;
 		}
 
-		if ( ! hello_elementor_display_header_footer() ) {
+		if (!hello_elementor_display_header_footer()) {
 			return;
 		}
 
 		require get_template_directory() . '/includes/customizer-functions.php';
 	}
 }
-add_action( 'init', 'hello_elementor_customizer' );
+add_action('init', 'hello_elementor_customizer');
 
-if ( ! function_exists( 'hello_elementor_check_hide_title' ) ) {
+if (!function_exists('hello_elementor_check_hide_title')) {
 	/**
 	 * Check whether to display the page title.
 	 *
@@ -245,29 +253,148 @@ if ( ! function_exists( 'hello_elementor_check_hide_title' ) ) {
 	 *
 	 * @return bool
 	 */
-	function hello_elementor_check_hide_title( $val ) {
-		if ( defined( 'ELEMENTOR_VERSION' ) ) {
-			$current_doc = Elementor\Plugin::instance()->documents->get( get_the_ID() );
-			if ( $current_doc && 'yes' === $current_doc->get_settings( 'hide_title' ) ) {
+	function hello_elementor_check_hide_title($val)
+	{
+		if (defined('ELEMENTOR_VERSION')) {
+			$current_doc = Elementor\Plugin::instance()->documents->get(get_the_ID());
+			if ($current_doc && 'yes' === $current_doc->get_settings('hide_title')) {
 				$val = false;
 			}
 		}
 		return $val;
 	}
 }
-add_filter( 'hello_elementor_page_title', 'hello_elementor_check_hide_title' );
+add_filter('hello_elementor_page_title', 'hello_elementor_check_hide_title');
 
 /**
  * BC:
  * In v2.7.0 the theme removed the `hello_elementor_body_open()` from `header.php` replacing it with `wp_body_open()`.
  * The following code prevents fatal errors in child themes that still use this function.
  */
-if ( ! function_exists( 'hello_elementor_body_open' ) ) {
-	function hello_elementor_body_open() {
+if (!function_exists('hello_elementor_body_open')) {
+	function hello_elementor_body_open()
+	{
 		wp_body_open();
 	}
 }
 
+
+
+
+
+
 require HELLO_THEME_PATH . '/theme.php';
 
+
 HelloTheme\Theme::instance();
+
+// Product/Variation ID => minimum quantity required
+function jg_min_rules(): array
+{
+	return [
+		711 => 10,  // Cheese Balls (example)
+		708 => 10,   // Coxinha (example)
+		86 => 10,  // Brigadeiro (example)
+	];
+}
+
+// ---------------- HELPERS ----------------
+function jg_cart_qty_for(int $target_id): int
+{
+	if (!WC()->cart)
+		return 0;
+	$qty = 0;
+	foreach (WC()->cart->get_cart() as $ci) {
+		$pid = (int) $ci['product_id'];
+		$vid = isset($ci['variation_id']) ? (int) $ci['variation_id'] : 0;
+		if ($pid === $target_id || $vid === $target_id) {
+			$qty += (int) $ci['quantity'];
+		}
+	}
+	return $qty;
+}
+
+// Each item: ['id'=>int,'name'=>string,'qty'=>int,'min'=>int]
+function jg_blocked_items(): array
+{
+	if (!WC()->cart)
+		return [];
+	$blocked = [];
+	foreach (jg_min_rules() as $id => $min) {
+		$qty = jg_cart_qty_for((int) $id);
+		if ($qty > 0 && $qty < (int) $min) {
+			$name = 'This product';
+			foreach (WC()->cart->get_cart() as $ci) {
+				$pid = (int) $ci['product_id'];
+				$vid = isset($ci['variation_id']) ? (int) $ci['variation_id'] : 0;
+				if ($pid === (int) $id || $vid === (int) $id) {
+					$name = $ci['data']->get_name();
+					break;
+				}
+			}
+			$blocked[] = ['id' => (int) $id, 'name' => $name, 'qty' => (int) $qty, 'min' => (int) $min];
+		}
+	}
+	return $blocked;
+}
+
+function jg_has_block(): bool
+{
+	return !empty(jg_blocked_items());
+}
+
+// ---------------- CART PROCEED BUTTON (no custom messages here) ----------------
+add_action('init', function () {
+	remove_action('woocommerce_proceed_to_checkout', 'woocommerce_button_proceed_to_checkout', 20);
+
+	add_action('woocommerce_proceed_to_checkout', function () {
+		$blocked = jg_blocked_items();
+		if (!empty($blocked)) {
+			// Disabled-looking anchor (Elementor styles still apply)
+			$label = esc_html__('Proceed to checkout', 'woocommerce');
+			echo '<a class="checkout-button button alt disabled" href="#" aria-disabled="true" style="opacity:.5;pointer-events:none;cursor:not-allowed">'
+				. $label
+				. '</a>';
+			// No extra messages here; the standard Woo notices bar will show them.
+		} else {
+			if (function_exists('woocommerce_button_proceed_to_checkout')) {
+				woocommerce_button_proceed_to_checkout();
+			}
+		}
+	}, 20);
+});
+
+// Safety: keep user on Cart if something still tries to fetch checkout URL while blocked
+add_filter('woocommerce_get_checkout_url', function ($url) {
+	if (is_cart() && jg_has_block()) {
+		return wc_get_cart_url();
+	}
+	return $url;
+});
+
+// ---------------- CHECKOUT/CART VALIDATOR (single combined notice; run once) ----------------
+add_action('woocommerce_check_cart_items', function () {
+	static $ran = false;
+	if ($ran)
+		return;         // avoid duplicates if Woo triggers the hook twice
+	$ran = true;
+
+	if (!(is_cart() || is_checkout()))
+		return;
+
+	$blocked = jg_blocked_items();
+	if (empty($blocked))
+		return;
+
+	// Build one combined message (one notice, multiple lines)
+	$lines = [];
+	foreach ($blocked as $it) {
+		$lines[] = sprintf(
+			__('Minimum order for %1$s is %2$d units. You currently have %3$d.', 'your-td'),
+			$it['name'],
+			(int) $it['min'],
+			(int) $it['qty']
+		);
+	}
+	wc_add_notice(implode('<br>', array_map('esc_html', $lines)), 'error');
+}, 20);
